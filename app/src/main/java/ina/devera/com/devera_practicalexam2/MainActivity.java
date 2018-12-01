9023fe76e679001ad06e5f1d1846968975802a87
whalien7oh7 <2015081484@ust-ics.mygbiz.com>
2018-12-01 14:56:55 +0800

    Initial Commit

--- app/src/main/java/ina/devera/com/devera_practicalexam2/MainActivity.java
+++ app/src/main/java/ina/devera/com/devera_practicalexam2/MainActivity.java
@@ -2,55 +2,77 @@ package ina.devera.com.devera_practicalexam2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText exam1;
-    private EditText exam2;
-    private TextView Ave;
+    EditText exam1;
+    EditText exam2;
+    TextView Ave_text;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         exam1 = (EditText) findViewById(R.id.exam1);
         exam2 = (EditText) findViewById(R.id.exam2);
-        Ave = (TextView) findViewById(R.id.Ave);
+        Ave_text = (TextView) findViewById(R.id.Ave);
     }
 
     public void calculateave (View v) {
         String exam1Str = exam1.getText().toString();
         String exam2Str = exam2.getText().toString();
+        Float exam1Int = Float.parseFloat(exam1Str);
+        Float exam2Int = Float.parseFloat(exam2Str);
+        Float Ave = (exam1Int+exam2Int)/2;
+        Ave_text.setText(Float.toString(Ave));
+        writeToFile("Chicken Nuggets",exam1Str+exam2Str+Ave);
 
-        if (exam1Str != null && !"".equals(exam1Str)
-                && exam1Str != null  &&  !"".equals(exam1Str)) {
-            float exam1Value = Float.parseFloat(exam1Str);
-            float exam2Value = Float.parseFloat(exam2Str);
 
 
-            float Ave = (exam1Value + exam2Value) / 2;
-
-        }
 }
 
-    private void displayAve(float Ave) {
-        String aveLabel = "";
+    public  void writeToFile(String fileName, String body)
+    {
+        FileOutputStream fos = null;
 
-        aveLabel = Ave + "\n\n" + aveLabel;
+        try {
+            final File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DeVeraPracticalExam2/" );
 
-        Intent i = new Intent(this, MainActivity.class);
+            if (!dir.exists())
+            {
+                if(!dir.mkdirs()){
+                    Log.e("ALERT","could not create the directories");
+                }
+            }
 
-        String getrec=aveLabel;
+            final File myFile = new File(dir, fileName + ".txt");
 
-        Bundle bundle = new Bundle();
-
-        bundle.putString("stuff", getrec);
-
-        i.putExtras(bundle);
-
+            if (!myFile.exists())
+            {
+                myFile.createNewFile();
+            }
+            fos = new FileOutputStream(myFile);
+            fos.write(body.getBytes());
+            Toast.makeText(getBaseContext(), "Persist Successful",
+                    Toast.LENGTH_SHORT).show();
+            fos.close();
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
 
